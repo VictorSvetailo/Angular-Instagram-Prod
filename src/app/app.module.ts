ac53e5b004f9d99925e51031c5aa8a511a07f7d5
VictorSvetailo <victorioeve@gmail.com>
2022-12-13 10:18:21 +0300

    feat(start, test) first commit Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Test1Component} from './test1/test1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
